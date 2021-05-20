94cc869e8ea6fd8325df9b49e7b31bb1230f873a
juncho89 <chojun89@gmail.com>
2021-05-21 00:34:57 +0900

    add LGBM Algorithm
    add LGBM Algorithm

--- Tabular_Playground_Series/Tabular_Playground.py
+++ Tabular_Playground_Series/Tabular_Playground.py
@@ -15,6 +15,8 @@ feature = []
 feature.append("id")
 for i in range(50):
     feature.append("feature_%d"%i)
+
+
 label = ['target']
 
 x = train[feature]
@@ -46,13 +48,11 @@ plt.show()
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, stratify=y, random_state=34)
 
-
 # LR
 from sklearn.linear_model import LinearRegression
 line_fitter = LinearRegression()
 line_fitter.fit(x_train, y_train)
 y_pred = line_fitter.predict(x_test)
-
 
 from sklearn.linear_model import LinearRegression        ## 선형 회귀분석
 from sklearn.linear_model import LogisticRegression      ## 로지스틱 회귀분석
@@ -69,9 +69,9 @@ clf.score(x_test,y_test)
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(x_train, y_train)
-clf.predict(x_test)
+y_pred = clf.predict(x_test)
 clf.predict_proba(x_test)
-clf.score(x_test, y_test)
+clf.score(y_pred, y_test)
 
 
 prob = clf.predict_proba(test)
@@ -83,7 +83,45 @@ submission['Class_4'] = df[3]
 
 submission.to_csv('submission.csv', index=False)
 
+import xgboost as xgb
+xgb_clf = xgb.XGBClassifier(random_state=42,tree_method='gpu_hist',subsample=0.7)
+xgb_clf.fit(x_train, y_train, verbose=True)
 
+from lightgbm import LGBMClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import log_loss
+
+lgbm_clf = LGBMClassifier()
+lgbm_clf.fit(x_train, y_train)
+y_pred = lgbm_clf.predict_proba(x_test)
+log_loss(y_test, y_pred)
+
+
+
+
+prob = lgbm_clf.predict_proba(test)
+df = pd.DataFrame(data=prob)
+submission['Class_1'] = df[0]
+submission['Class_2'] = df[1]
+submission['Class_3'] = df[2]
+submission['Class_4'] = df[3]
+submission.to_csv('submission_lgbm.csv', index=False)
+
+
+
+
+clf.predict(x_test)
+clf.predict_proba(x_test)
+clf.score(x_test,y_test)
+
+
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, stratify=y)
+lgbm_clf = LGBMClassifier()
+lgbm_clf.fit(x_train, y_train)
+y_pred = lgbm_clf.predict_proba(x_test)
+log_loss(y_test, y_pred)
 
 
 
